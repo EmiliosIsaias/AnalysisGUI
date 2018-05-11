3f35bda864b26e810c85eac82683310eb265a835
Emilio Isaías-Camacho <emilio.isaias-camacho@fau.de>
2018-05-11 14:30:03 +0200

    Included the new file extension '*.smrx'
    The file explorer includes now the new file extension '*.smrx'

--- DataAnalysis.m
+++ DataAnalysis.m
@@ -81,7 +81,7 @@ function pushbutton1_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-[fnames pathname] = uigetfile({'*.mat';'*.smr'},'Choose file(s) to analyze:','multiselect','on');
+[fnames pathname] = uigetfile({'*.mat';'*.smr';'*.smrx'},'Choose file(s) to analyze:','multiselect','on');
 handles.FNAMES=fnames;
 handles.PATHNAME=pathname;
 
